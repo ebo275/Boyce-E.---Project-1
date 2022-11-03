06b95a20f2f7255a21a9ea791fa5a89b59b83e86
ebo275 <115588483+ebo275@users.noreply.github.com>
2022-11-03 13:51:30 -0700

    bio paragraph added

--- projects/app.js
+++ projects/app.js
@@ -1,5 +1,5 @@
 $( () => {
-
+//---------------------VARIABLES---------------------//
     const $openBtn = $('h1');
     const $modal = $('#modal');
     const $closeBtn = $('#close')
@@ -7,8 +7,13 @@ $( () => {
     const $sockShoe = $('#sockShoe')
     const $option1 = $('#option1')
     const $option2 = $('#option2')
+    const $bioButton = $('#bio-button')
+    const $bio = $('#bio')
 
 
+//---------------------FUNCTIONS---------------------//
+
+//---------------------MODAL---------------------//
 
     const openModal = () => {
         $modal.css('display', 'flex');
@@ -26,11 +31,25 @@ $( () => {
     const sockShoe = () => {
         $option2.toggle();    
     }
+
+//---------------------BIO---------------------//
+
+    const bioSlide = () => {
+        $bio.css('display', 'flex');
+    }
+    const bioHide = () => {
+        $bio.css('display', 'none');
+    }
+
     
+    
+//---------------EVENT LISTENERS/INVOCATIONS-----------------//
 
     $openBtn.on('click', openModal)
     $closeBtn.on('click', closeModal)
     $sockSock.on('click', sockSock)
     $sockShoe.on('click', sockShoe)
+    $bioButton.mouseenter(bioSlide)
+    $bioButton.mouseleave(bioHide)
 
 })
